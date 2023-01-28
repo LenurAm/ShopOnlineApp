0a6834f4f7ac2c9ff61a407f23d523c7ce1ad270
Lenur <93513097+LenurAm@users.noreply.github.com>
2023-01-29 00:16:19 +1300

    change the server settings

---
+++ ShopOnline.Api/Migrations/20230127114256_ShopOnlineApp.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopOnline.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ShopOnlineApp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductCategory",
+                table: "ProductCategory");
+
+            migrationBuilder.RenameTable(
+                name: "ProductCategory",
+                newName: "ProductCategories");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductCategories",
+                table: "ProductCategories",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductCategories",
+                table: "ProductCategories");
+
+            migrationBuilder.RenameTable(
+                name: "ProductCategories",
+                newName: "ProductCategory");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductCategory",
+                table: "ProductCategory",
+                column: "Id");
+        }
+    }
+}
